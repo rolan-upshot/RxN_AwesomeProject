645c0c6d900ccfa7756576e472434135034adcff
rolan-upshot <rolan@upshot.global>
2022-01-06 21:25:20 +0800

    running hello world

---
+++ app/App.js
@@ -0,0 +1,24 @@
+/* eslint-disable react-native/no-inline-styles */
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow strict-local
+ */
+import React from 'react';
+import {Text, View} from 'react-native';
+
+const HelloWorldApp = () => {
+  return (
+    <View
+      style={{
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+      }}>
+      <Text>Hello, world!</Text>
+    </View>
+  );
+};
+export default HelloWorldApp;
